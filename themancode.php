cd5abba9380509d0fddc41622e8832d0b546f500
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-12-16 03:51:13 +0530

    Update themancode.php

--- themancode.php
+++ themancode.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "fastcart";
+$servername = "sql200.infinityfree.com";
+$username = "if0_35626457";
+$password = "uACZUlW2D8Unhz";
+$db = "if0_35626457_fastcart";
 // Create connection
 $con = new mysqli($servername, $username, $password,$db);
 
